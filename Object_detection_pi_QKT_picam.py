344f8aaff243bc1d8a37b28eb56448e8e0f5a90a
Quang Khoi Tran <30682277+darkshilva@users.noreply.github.com>
2019-03-15 11:27:49 +0700

    Add files via upload

--- Object_detection_pi_QKT_picam.py
+++ Object_detection_pi_QKT_picam.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[170]:
 
 
-######## Camera Object Detection Using Tensorflow #########
+######## Picamera Object Detection Using Tensorflow #########
 #
 # Author: Quang Khoi Tran
 # Date: 11/03/19
 # Description: 
 # This program uses TensorFlow to perform object detection.
-# It peforms object detection on a webcam feed.
+# It loads the classifier uses it to perform object detection on a Picamera feed.
 # It draws boxes and scores around the objects of interest in each frame from
-# the camera. 
-
+# the Picamera. 
 ## Some of the code is copied from Google's example at
 ## https://github.com/tensorflow/models/blob/master/research/object_detection/object_detection_tutorial.ipynb
 
@@ -21,15 +20,15 @@
 ## https://github.com/datitran/object_detector_app/blob/master/object_detection_app.py
 
 
-# In[2]:
+# In[171]:
 
 
 # Import packages
 import os
 import cv2
 import numpy as np
-#from picamera.array import PiRGBArray
-#from picamera import PiCamera
+from picamera.array import PiRGBArray
+from picamera import PiCamera
 import tensorflow as tf
 import argparse
 import sys
@@ -37,7 +36,7 @@ import time
 import psutil as sys_info 
 
 
-# In[3]:
+# In[172]:
 
 
 # Set up images resolution
@@ -47,12 +46,12 @@ IM_HEIGHT = 720
 #IM_HEIGHT = 480   
 
 
-# In[4]:
+# In[173]:
 
 
 # Select camera type (trying feature: if user enters --webcam when calling this script,
 # a webcam will be used)
-#camera_type = 'picamera'
+camera_type = 'picamera'
 ''' #testing feature to run program from terminal with --webcam
 parser = argparse.ArgumentParser()
 parser.add_argument('--usbcam', help='Use a USB webcam instead of picamera',
@@ -61,12 +60,12 @@ args = parser.parse_args()
 if args.usbcam:
     camera_type = 'webcam'
  '''
-camera_type='webcam'
+#camera_type='webcam'
 # This is needed since the working directory is the object_detection folder.
 sys.path.append('..')
 
 
-# In[5]:
+# In[174]:
 
 
 # Import utilites
@@ -74,7 +73,7 @@ from utils import label_map_util
 from utils import visualization_utils as vis_util
 
 
-# In[6]:
+# In[175]:
 
 
 # Name of the directory containing the object detection module we're using
@@ -94,7 +93,7 @@ PATH_TO_LABELS = os.path.join(CWD_PATH,'data','mscoco_label_map.pbtxt')
 NUM_CLASSES = 90
 
 
-# In[7]:
+# In[176]:
 
 
 ## Load the label map.
@@ -118,7 +117,7 @@ with detection_graph.as_default():
     sess = tf.Session(graph=detection_graph)
 
 
-# In[8]:
+# In[177]:
 
 
 # Define input and output tensors (i.e. data) for the object detection classifier
@@ -139,7 +138,7 @@ detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
 num_detections = detection_graph.get_tensor_by_name('num_detections:0')
 
 
-# In[9]:
+# In[178]:
 
 
 
@@ -149,13 +148,13 @@ freq = cv2.getTickFrequency()
 font = cv2.FONT_HERSHEY_SIMPLEX
 
 
-# In[ ]:
+# In[179]:
 
 
 
 
 
-# In[10]:
+# In[180]:
 
 
 #Initialize time logging
@@ -167,38 +166,35 @@ sys_info_cpu=sys_info.cpu_percent()
 sys_info_ram=sys_info.virtual_memory()[2] #2 = percent
 
 
-# In[ ]:
+# In[181]:
 
 
+# piCamera
+if camera_type == 'picamera':
+    # Initialize Picamera and grab reference to the raw capture
+    camera = PiCamera()
+    camera.resolution = (IM_WIDTH,IM_HEIGHT)
+    camera.framerate = 10
+    rawCapture = PiRGBArray(camera, size=(IM_WIDTH,IM_HEIGHT))
+    rawCapture.truncate(0)
 
-
-
-# In[11]:
-
-
-if camera_type == 'webcam':
-    # Initialize webcam feed, 1= back camera, 0 = front camera (surfacebook 2)
-    camera = cv2.VideoCapture(1)
-    #camera = cv2.VideoCapture('720p.mp4')
-    #replace with video.mp4 for offline processing
-    ret = camera.set(3,IM_WIDTH)
-    ret = camera.set(4,IM_HEIGHT)
-    
     #Initialize output video
     # VideoWriter is the responsible of creating a copy of the video
     # used for the detections but with the detections overlays. Keep in
     # mind the frame size has to be the same as original video.
-    out = cv2.VideoWriter('object_detected_result_webcam.avi', cv2.VideoWriter_fourcc(*'XVID'), 10, (IM_WIDTH, IM_HEIGHT))
+    out = cv2.VideoWriter('object_detected_result_pi.avi', cv2.VideoWriter_fourcc(*'XVID'), 10, (IM_WIDTH, IM_HEIGHT))
     #VideoWriter('name.avi,codec,FPS,(width,height)')
-    while(True):
+    
+    for frame1 in camera.capture_continuous(rawCapture, format="bgr",use_video_port=True):
 
         t1 = cv2.getTickCount()
-
+        
         # Acquire frame and expand frame dimensions to have shape: [1, None, None, 3]
         # i.e. a single-column array, where each item in the column has the pixel RGB value
-        ret, frame = camera.read()
+        frame = np.copy(frame1.array)
+        frame.setflags(write=1)
         frame_expanded = np.expand_dims(frame, axis=0)
-        
+
         # Perform the actual detection by running the model with the image as input
         (boxes, scores, classes, num) = sess.run(
             [detection_boxes, detection_scores, detection_classes, num_detections],
@@ -213,20 +209,22 @@ if camera_type == 'webcam':
             category_index,
             use_normalized_coordinates=True,
             line_thickness=8,
-            min_score_thresh=0.45)
+            min_score_thresh=0.40)
 
         cv2.putText(frame,"FPS: {0:.2f}".format(frame_rate),(30,50),font,1,(255,255,0),2,cv2.LINE_AA)
         cv2.putText(frame,time_string,(250,50),font,1,(255,255,0),2,cv2.LINE_AA)
         cv2.putText(frame,"CPU: {0:.2f}".format(sys_info_cpu),(750,50),font,1,(255,255,0),2,cv2.LINE_AA)
         cv2.putText(frame,"RAM: {0:.2f}".format(sys_info_ram),(950,50),font,1,(255,255,0),2,cv2.LINE_AA)
+     
+
         # All the results have been drawn on the frame, so it's time to display it.
-        cv2.imshow('Object Detector QKT v0.1', frame)
+        cv2.imshow('Object detector', frame)
         #write the result video
         # https://www.learnopencv.com/why-does-opencv-use-bgr-color-format/
         #color_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         #output_rgb = cv2.cvtColor(color_frame, cv2.COLOR_RGB2BGR)
         out.write(frame)
-
+        
         t2 = cv2.getTickCount()
         time1 = (t2-t1)/freq
         frame_rate = 1/time1 #update Frame rate
@@ -238,12 +236,17 @@ if camera_type == 'webcam':
         # Press 'q' to quit
         if cv2.waitKey(1) == ord('q'):
             break
-            
-    out.release()
-    camera.release()
+
+        rawCapture.truncate(0)
+
+    out.realease()
+    camera.close()
 
 
-cv2.destroyAllWindows()
+# In[182]:
+
+
+
 
 
 # In[ ]:
